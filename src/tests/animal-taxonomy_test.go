b6c31f16994e59c10ccd1db39a4b90b5ed73b711
Mike Bishop <tend2dv8@gmail.com>
2020-09-07 16:14:04 -0500

    Added styling to the PlantUML diagram.

--- src/tests/animal-taxonomy_test.go
+++ src/tests/animal-taxonomy_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	. "operators"
 	"os"
+	"os/user"
 	. "taxonomy"
 	"testing"
 )
@@ -24,19 +25,19 @@ func TestSampleAnimalTaxonomy(t *testing.T) {
 	marisaSeesClaireTheElephant()
 	marisaRealizesThatSomeElephantsHaveTusksAndSomeElephantsDoNot()
 
-	generatePlantUML(*animalTaxonomy, "/home/mike/IdeaProjects/inductive-taxonomy/src/resources/taxonomy.puml")
+	generatePlantUML(*animalTaxonomy, homeDirectory()+"/IdeaProjects/inductive-taxonomy/src/resources/taxonomy.puml")
 
-	checkPrototype(t, animalTaxonomy, "Mammal", "Animal")
-	checkPrototype(t, animalTaxonomy, "Reptile", "Animal")
-	checkPrototype(t, animalTaxonomy, "Elephant", "Mammal")
-	checkPrototype(t, animalTaxonomy, "Giraffe", "Mammal")
-	checkPrototype(t, animalTaxonomy, "ElephantWithTusks", "Elephant")
-	checkPrototype(t, animalTaxonomy, "ElephantWithoutTusks", "Elephant")
-	checkPrototype(t, animalTaxonomy, "Stanley", "Reptile")
-	checkPrototype(t, animalTaxonomy, "Clyde", "ElephantWithTusks")
-	checkPrototype(t, animalTaxonomy, "Chloe", "ElephantWithoutTusks")
-	checkPrototype(t, animalTaxonomy, "Claire", "ElephantWithoutTusks")
-	checkPrototype(t, animalTaxonomy, "Jerry", "Giraffe")
+	checkPrototype(t, *animalTaxonomy, "Mammal", "Animal")
+	checkPrototype(t, *animalTaxonomy, "Reptile", "Animal")
+	checkPrototype(t, *animalTaxonomy, "Elephant", "Mammal")
+	checkPrototype(t, *animalTaxonomy, "Giraffe", "Mammal")
+	checkPrototype(t, *animalTaxonomy, "ElephantWithTusks", "Elephant")
+	checkPrototype(t, *animalTaxonomy, "ElephantWithoutTusks", "Elephant")
+	checkPrototype(t, *animalTaxonomy, "Stanley", "Reptile")
+	checkPrototype(t, *animalTaxonomy, "Clyde", "ElephantWithTusks")
+	checkPrototype(t, *animalTaxonomy, "Chloe", "ElephantWithoutTusks")
+	checkPrototype(t, *animalTaxonomy, "Claire", "ElephantWithoutTusks")
+	checkPrototype(t, *animalTaxonomy, "Jerry", "Giraffe")
 	assert.True(t, animalTaxonomy.GetObject("Clyde").Get("hasTusks").(bool))
 	assert.False(t, animalTaxonomy.GetObject("Chloe").Get("hasTusks").(bool))
 	assert.Equal(t, "warm", animalTaxonomy.GetObject("Jerry").Get("blooded"))
@@ -136,38 +137,57 @@ func applyOperatorsToTaxonomy(operators []Operator, taxonomy *Taxonomy) {
 	}
 }
 
-func checkPrototype(t *testing.T, taxonomy *Taxonomy, instanceName string, prototypeName string) {
+func checkPrototype(t *testing.T, taxonomy Taxonomy, instanceName string, prototypeName string) {
 	assert.Equal(t, taxonomy.GetObject(prototypeName), taxonomy.GetObject(instanceName).Prototype())
+}
+
+func homeDirectory() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return u.HomeDir
 }
 
 func generatePlantUML(taxonomy Taxonomy, filePath string) {
 	file, err := os.Create(filePath)
-	checkFileError(err)
+	if err != nil {
+		panic(err)
+	}
 	defer file.Close()
 
-	_, err = file.WriteString("@startuml\n")
-	checkFileError(err)
-
-	for _, prototypeRelationship := range taxonomy.GetPrototypeRelationships() {
-		_, err := file.WriteString(prototypeRelationship.Prototype().Name() + " <|-- " + prototypeRelationship.Instance().Name() + "\n")
-		checkFileError(err)
-	}
+	writeToFile(file, "@startuml")
+	writeToFile(file, "skinparam shadowing false")
+	writeToFile(file, "skinparam roundcorner 10")
+	writeToFile(file, "skinparam backgroundColor transparent")
+	writeToFile(file, "skinparam arrowColor Ivory")
+	writeToFile(file, "skinparam object {")
+	writeToFile(file, "  BackgroundColor #B6CEE2")
+	writeToFile(file, "  BorderColor #4A5761")
+	writeToFile(file, "  FontSize 14")
+	writeToFile(file, "  FontStyle bold")
+	writeToFile(file, "  AttributeFontSize 12")
+	writeToFile(file, "}")
 
 	for _, object := range taxonomy.GetObjects() {
+		writeToFile(file, "object "+object.Name())
 		for key, val := range object.Properties() {
 			valueString := fmt.Sprintf("%v", val)
-			_, err := file.WriteString(object.Name() + " : " + key + " = " + valueString + "\n")
-			checkFileError(err)
+			writeToFile(file, object.Name()+" : "+key+" = "+valueString)
 		}
 	}
 
-	_, err = file.WriteString("@enduml\n")
-	checkFileError(err)
+	for _, prototypeRelationship := range taxonomy.GetPrototypeRelationships() {
+		writeToFile(file, prototypeRelationship.Prototype().Name()+" <|-- "+prototypeRelationship.Instance().Name())
+	}
+
+	writeToFile(file, "@enduml")
 
 	file.Sync()
 }
 
-func checkFileError(err error) {
+func writeToFile(file *os.File, text string) {
+	_, err := file.WriteString(text + "\n")
 	if err != nil {
 		panic(err)
 	}
